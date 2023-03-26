3e9cf3ec27590ad7e3685d35e7813e4498b51c85
IvanGorbunov <ivan_0110@mail.ru>
2023-03-26 14:14:08 +0300

    added ordering

--- books_app/store/views.py
+++ books_app/store/views.py
@@ -1,5 +1,4 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+
 from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
 
@@ -11,10 +10,13 @@ from store.serializers import BooksSerializer
 class BooksViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
-    filter_backends = (DjangoFilterBackend, SearchFilter,)
     filterset_class = BookFilter
     search_fields = (
         'name',
+        'author_name',
+    )
+    ordering_fields = (
+        'price',
         'author_name',
     )
 
@@ -23,9 +25,11 @@ class BooksListViewSet(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
     filterset_class = BookFilter
-    filter_backends = (DjangoFilterBackend, SearchFilter,)
     search_fields = (
         'name',
         'author_name',
     )
-
+    ordering_fields = (
+        'price',
+        'author_name',
+    )
